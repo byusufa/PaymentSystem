ae676d102e85a3139364d3e5504d3a5e82bda45b
byusufa <jozef12079104293@gmail.com>
2025-08-15 18:41:47 +0500

    basic init commit

---
+++ src/main/java/uz/pdp/paymentsystemforcafe/PaymentSystemForCafeApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.paymentsystemforcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemForCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemForCafeApplication.class, args);
+    }
+
+}
