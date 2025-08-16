1a277c71a4ec66999bed51ab07763b737f28f929
byusufa <jozef12079104293@gmail.com>
2025-08-16 16:46:35 +0500

    securityconfig,webconfig commit

---
+++ src/main/java/uz/pdp/paymentsystemforcafe/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package uz.pdp.paymentsystemforcafe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uz.pdp.paymentsystemforcafe.filter.MyFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomUserDetailsService customUserDetailsService, MyFilter myFilter) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+//      http.cors(AbstractHttpConfigurer::disable);
+        http.cors(Customizer.withDefaults());
+        http.authorizeHttpRequests(req ->
+                req
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/login", "/images/**", "/api/file/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/category/**", "/api/product/**", "/api/product/category/**", "/api/order", "/api/orderItem/**").hasRole("CASHIER")
+                        .requestMatchers(HttpMethod.POST, "/api/order/**").hasRole("CASHIER")
+                        .requestMatchers("/api/category/**", "/api/product/**", "/api/order/**","/api/logout/**","/api/report/**").hasRole("ADMIN")
+                        .requestMatchers("/api/super_admin/**").hasRole("SUPER_ADMIN")
+                        .anyRequest().authenticated());
+
+        http.userDetailsService(customUserDetailsService);
+        http.addFilterBefore(myFilter, UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CustomUserDetailsService customUserDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
+    }
+
+}
