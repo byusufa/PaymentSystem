c1316ae4ed995c5eccb98d9f7879a41838a32a9f
byusufa <jozef12079104293@gmail.com>
2025-08-16 16:48:31 +0500

    jwtservice,myfilter commit

---
+++ src/main/java/uz/pdp/paymentsystemforcafe/filter/MyFilter.java
@@ -0,0 +1,49 @@
+package uz.pdp.paymentsystemforcafe.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import uz.pdp.paymentsystemforcafe.entity.User;
+import uz.pdp.paymentsystemforcafe.repo.UserRepository;
+import uz.pdp.paymentsystemforcafe.jwtservice.JwtService;
+
+import java.io.IOException;
+
+@Component
+public class MyFilter extends OncePerRequestFilter {
+    
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public MyFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            if (jwtService.validate(token)) {
+                String username = jwtService.getUserName(token);
+                User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+                var auth = new UsernamePasswordAuthenticationToken(
+                        user, //user.getUsername
+                        null,
+                        user.getRoles()
+                );
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
+
