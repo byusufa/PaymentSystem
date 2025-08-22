4e3b46a3e9539589c5af83ef3d7405009979e4b6
byusufa <jozef12079104293@gmail.com>
2025-08-22 11:34:58 +0500

    AttachmentRepository commit

--- src/main/java/uz/pdp/paymentsystemforcafe/repo/AttachmentRepository.java
+++ src/main/java/uz/pdp/paymentsystemforcafe/repo/AttachmentRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.paymentsystemforcafe.entity.Attachment;
 
 public interface AttachmentRepository extends JpaRepository<Attachment, Integer> {
-
 }
