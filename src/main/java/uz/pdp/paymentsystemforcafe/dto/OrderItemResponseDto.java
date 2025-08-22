c04e6f41b5596dcea637bdd76c8b548054d89ea9
byusufa <jozef12079104293@gmail.com>
2025-08-22 11:39:57 +0500

    OrderItemResponseDto commit

---
+++ src/main/java/uz/pdp/paymentsystemforcafe/dto/OrderItemResponseDto.java
@@ -0,0 +1,18 @@
+package uz.pdp.paymentsystemforcafe.dto;
+
+import lombok.*;
+
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemResponseDto {
+    private UUID id;
+    private Integer count;
+    private UUID orderId;
+    private Integer productId;
+
+}
