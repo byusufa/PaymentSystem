a1bd94707c1fc0c3adbf7b9aa18097a24be39700
byusufa <jozef12079104293@gmail.com>
2025-08-22 15:44:09 +0500

    OrderService commit

--- src/main/java/uz/pdp/paymentsystemforcafe/service/OrderService.java
+++ src/main/java/uz/pdp/paymentsystemforcafe/service/OrderService.java
@@ -13,6 +13,7 @@ import uz.pdp.paymentsystemforcafe.repo.OrderRepository;
 import uz.pdp.paymentsystemforcafe.repo.ProductRepository;
 
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,7 +24,35 @@ public class OrderService {
     private final ProductRepository productRepository;
 
 
-    public Order addOrder(List<OrderItemRequestDto> orderItemRequestDto) {
+    public OrderResponseDto addOrder(List<OrderItemRequestDto> orderItemRequestDto) {
+
+//        if (orderItemRequestDto == null || orderItemRequestDto.isEmpty()) {
+//            throw new RuntimeException("Savatcha bo‘sh. Order yaratib bo‘lmaydi.");
+//        }
+//
+//        YearMonth currentMonth = YearMonth.now();
+//        int year = currentMonth.getYear();
+//        int month = currentMonth.getMonthValue();
+//        Integer maxOrderNumber = orderRepository.findMaxOrderNumberByMonth(year, month).orElse(0);
+//        Order order = new Order();
+//        order.setOrderNumber(maxOrderNumber + 1);
+//        order.setOrderYear(year);
+//        order.setOrderMonth(month);
+//
+//        for (OrderItemRequestDto item : orderItemRequestDto) {
+//            OrderItem orderItem = new OrderItem();
+//            Product product = productRepository.findById(item.getId()).orElseThrow(()
+//                    -> new RuntimeException("Product not found"));
+//            orderItem.setProduct(product);
+//            orderItem.setOrder(order);
+//            orderItem.setCount(item.getCount());
+//            order.getOrderItems().add(orderItem);
+//
+//        }
+//
+//        orderRepository.save(order);
+//        return order;
+
         if (orderItemRequestDto == null || orderItemRequestDto.isEmpty()) {
             throw new RuntimeException("Savatcha bo‘sh. Order yaratib bo‘lmaydi.");
         }
@@ -48,8 +77,25 @@ public class OrderService {
 
         }
 
-        orderRepository.save(order);
-        return order;
+        Order saveOrder = orderRepository.save(order);
+        return OrderResponseDto.builder()
+                .id(saveOrder.getId())
+                .orderNumber(saveOrder.getOrderNumber())
+                .orderYear(saveOrder.getOrderYear())
+                .orderMonth(saveOrder.getOrderMonth())
+                .localDateTime(saveOrder.getLocalDateTime())
+                .orderItems(
+                        saveOrder.getOrderItems().stream()
+                                .map(orderItem -> OrderItemResponseDto.builder()
+                                        .id(orderItem.getId())
+                                        .count(orderItem.getCount())
+                                        .orderId(saveOrder.getId()) // yoki orderItem.getOrder().getId()
+                                        .productId(orderItem.getProduct().getId())
+                                        .build()
+                                )
+                                .toList()
+                )
+                .build();
     }
 
     public List<OrderPro> getAllOrders() {
