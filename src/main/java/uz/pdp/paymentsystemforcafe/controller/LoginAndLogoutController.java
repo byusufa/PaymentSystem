334acf718f94176285ee7f9464ad9ae24c5703a1
byusufa <jozef12079104293@gmail.com>
2025-08-23 20:03:44 +0500

    LoginAndLogoutController commit

--- src/main/java/uz/pdp/paymentsystemforcafe/controller/LoginAndLogoutController.java
+++ src/main/java/uz/pdp/paymentsystemforcafe/controller/LoginAndLogoutController.java
@@ -1,5 +1,7 @@
 package uz.pdp.paymentsystemforcafe.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,22 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 import uz.pdp.paymentsystemforcafe.dto.LoginDto;
 import uz.pdp.paymentsystemforcafe.entity.User;
 import uz.pdp.paymentsystemforcafe.jwtservice.JwtService;
+import uz.pdp.paymentsystemforcafe.jwtservice.TokenBlacklistService;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("/login")
+@RequestMapping("/api")
 @RestController
-public class LoginController {
+public class LoginAndLogoutController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final TokenBlacklistService blacklistService;
 
-    public LoginController(AuthenticationManager authenticationManager, JwtService jwtService) {
+    public LoginAndLogoutController(AuthenticationManager authenticationManager, JwtService jwtService, TokenBlacklistService blacklistService) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.blacklistService = blacklistService;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public HttpEntity<?> login(@RequestBody LoginDto loginDto) {
         UsernamePasswordAuthenticationToken authentication
                 = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
@@ -40,4 +45,18 @@ public class LoginController {
         return ResponseEntity.ok(response);
 
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7); // faqat token qismi
+            blacklistService.add(token); // blacklistga qo‘shish
+        }
+
+        return ResponseEntity.ok("Logout successfully ✅");
+    }
+
+
 }
