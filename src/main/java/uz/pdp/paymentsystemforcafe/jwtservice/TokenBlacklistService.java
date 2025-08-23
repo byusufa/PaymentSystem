7e9a294eab1d4ac4ec113587bd1e43435f4d8aa9
byusufa <jozef12079104293@gmail.com>
2025-08-23 20:02:28 +0500

    TokenBlacklistService commit

---
+++ src/main/java/uz/pdp/paymentsystemforcafe/jwtservice/TokenBlacklistService.java
@@ -0,0 +1,19 @@
+package uz.pdp.paymentsystemforcafe.jwtservice;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class TokenBlacklistService {
+    private final Set<String> blacklist = new HashSet<>();
+
+    public void add(String token) {
+        blacklist.add(token);
+    }
+
+    public boolean contains(String token) {
+        return blacklist.contains(token);
+    }
+}
